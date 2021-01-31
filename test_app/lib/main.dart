5c50a42fe9c89a908b1a8d1c9d8de80698a850d6
Jeemin <jeeminjameshan@gmail.com>
2021-01-30 18:04:58 -0600

    Uploading testing app
    This is a flutter app with a variable temp that outputs 'Hello' to the device screen

---
+++ test_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var temp = 'Hello';
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(temp),
+      ),
+    ),
+  );
+}
