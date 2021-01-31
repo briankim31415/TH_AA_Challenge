63d20fa6f087115a384b88b31401b89772f689d9
juwon0502 <juwon.lee@utexas.edu>
2021-01-31 00:29:16 -0600

    kept app.py

--- app.py
+++ app.py
@@ -24,15 +24,15 @@ def home():
     if request.method == "POST":
         name = request.form['name']
         session['name'] = name
-        flight_num = request.form['flight_num']
-        session['flight_num'] = flight_num
+        flight_num = request.form['flight-number']
+        session['flight-number'] = flight_num
         print(name, flight_num)
         return render_template('next_page.html')
     # return "Hello World"
     # url = urllib.request.urlopen('https://aa-flight.herokuapp.com/flights?date=2020-01-01&origin=DFW').read()
     # flight_json = json.loads(url)
     # return flight_json[0]['flightNumber']
-    return render_template('temp_index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
