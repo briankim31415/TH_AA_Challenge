acf970b5501efe35c692f458713e23f6a1388fca
juwon0502 <juwon.lee@utexas.edu>
2021-01-30 18:41:56 -0600

    corrected file locations

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import requests
+import urllib.request
+import json
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+@app.route('/', methods=['GET'])
+def home():
+    # return "Hello World"
+    # url = urllib.request.urlopen('https://aa-flight.herokuapp.com/flights?date=2020-01-01&origin=DFW').read()
+    # flight_json = json.loads(url)
+    # return flight_json[0]['flightNumber']
+    return render_template('temp_index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
